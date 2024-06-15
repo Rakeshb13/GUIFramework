e7c5f2d489126631a08158df18ec283c705606eb
Ajay Yadav <ajay.yadav.210698@gmail.com>
2024-06-15 15:33:36 +0530

    Updates By Ajay

--- GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/LoginPage.java
+++ GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/LoginPage.java
@@ -1,66 +1,32 @@
 package com.comcast.crm.objectrepositoryutility;
 
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import com.comcast.crm.generic.webdriverutility.WebDriverUtility;
-
-/**
- * 
- * @author Deepak
- * 
- * Contains Login page elements & business lib like login()
- *
- */  
-public class LoginPage extends WebDriverUtility{                              // Rule-1  create a separte java class
-                           
+public class LoginPage {
+	
 	WebDriver driver;
-	 public LoginPage(WebDriver driver) {             //Rule 3 : Object Initialization
-		 this.driver = driver;
-		 PageFactory.initElements(driver, this);
-	 }
-	                           
-	@FindBy(name="user_name")                        // Rule-2 Object Creation
-	private WebElement usernameEdt;
 	
-	@FindBy(name="user_password")
-	private WebElement passwordEdt;
-	
-	@FindBy(id = "submitButton")
-	private WebElement loginBtn;
-	     
-	
-	                                             
-	public WebElement getUsernameEdt() {            //Rule-4 : Object Encapsulation
-		return usernameEdt;                        //Rule-5 : Object Utilization
+	public LoginPage(WebDriver driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
 	}
-
-	public WebElement getPasswordEdt() {
-		return passwordEdt;
+	@FindBy(id = "username")
+	private WebElement UsenameTB;
+	
+	@FindBy(id = "password")
+	private WebElement PasswordTB;
+	
+	@FindBy(xpath = "//input[@class='button']")
+	private WebElement LoginBtn;
+	
+	public void login(String url,String userName , String password) {
+		driver.get(url);
+		UsenameTB.sendKeys(userName);
+		PasswordTB.sendKeys(password);
+		LoginBtn.click();
 	}
-
-	public WebElement getLoginBtn() {
-		return loginBtn;
-	}
-	
-/**
- *  login to application based username , password , url argumnets 
- * @param url
- * @param username
- * @param password
- */
-	 public void loginToapp(String url , String username , String password) {
-		 waitForPageToLoad(driver);
-		 driver.get(url);	
-		// driver.manage().window().maximize();
-		 usernameEdt.sendKeys(username);
-		 passwordEdt.sendKeys(password);
-		 loginBtn.click();
-	 }
-	
-	
-}
+  }
